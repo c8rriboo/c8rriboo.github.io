fa0a9af0f63b7471813c3947f35e5a2363542594
c8rriboo <c8rriboo@gmail.com>
2018-11-04 00:34:13 +0100

    Another one

--- src/js/app.js
+++ src/js/app.js
@@ -10396,6 +10396,12 @@ $(document).ready(function() {
 
 	console.log('ready');
 
+	resetGRadientScroll();
+
+	function resetGRadientScroll() {
+		$('.gradient-scroll').removeClass('gradient-scroll').addClass('gradient-scroll');
+	}
+
 	var container = document.getElementById('c8rriboo');
 
 	if (container.addEventListener) {
@@ -10415,9 +10421,24 @@ $(document).ready(function() {
 
 		var scroll_value = container.scrollLeft + (200 * (-delta));
 
-		TweenLite.to('#c8rriboo', .5, {scrollTo:{x: scroll_value}});
+		if (scroll_value < 0) {
+			scroll_value = 0;
+		}
+
+		TweenMax.to('#c8rriboo', .15, {scrollTo: {x: scroll_value}, onComplete: isPastTheEnd, onCompleteParams: [scroll_value]});
 
 		return false;
+	}
+
+	function isPastTheEnd(scroll_value) {
+		var first_section = $('section').first(),
+			width = first_section.outerWidth();
+
+		if (scroll_value > width) {
+			scroll_value = scroll_value - width;
+
+			TweenLite.to('#c8rriboo', -1, {scrollTo:{x: scroll_value}});
+		}
 	}
 
 	window.setInterval(function(){
@@ -10427,7 +10448,7 @@ $(document).ready(function() {
 	var prev_slide_container = 10;
 
 	function updateRandomSlide() {
-		var slide_containers = $('.slides-container').length;
+		var slide_containers = $('section').first().find('.slides-container').length;
 
 		var next_slide_container = Math.floor(Math.random() * slide_containers);
 
@@ -10441,17 +10462,19 @@ $(document).ready(function() {
 	function nextSlide(index) {
 		prev_slide_container = index;
 
-		var slide_container = $($('.slides-container')[index]);
+		$('section').each(function() {
+			var slide_container = $($(this).find('.slides-container')[index]);
 
-		var active_slide = slide_container.find('.slide.active');
-		var next_slide = active_slide.next();
+			var active_slide = slide_container.find('.slide.active');
+			var next_slide = active_slide.next();
 
-		if (next_slide.length < 1) {
-			next_slide = slide_container.find('.slide').eq(0);
-		}
+			if (next_slide.length < 1) {
+				next_slide = slide_container.find('.slide').eq(0);
+			}
 
-		active_slide.removeClass('active')
-		next_slide.addClass('active');
+			active_slide.removeClass('active')
+			next_slide.addClass('active');
+		});
 	}
 
 });
